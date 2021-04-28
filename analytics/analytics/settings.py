6cf73fccfca7c206ce7923dff6bfc95d6ab6cff5
Khaled Mebhah <gk_mebhah@esi.dz>
2021-04-28 16:44:28 +0000

    Hide the django secret key

--- analytics/analytics/settings.py
+++ analytics/analytics/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-rv!k*wv#f-ox#!btjq(_j)ce&=(g!2-9a7!q86k%4jg=^x)@_7'
+SECRET_KEY = os.environ['SECRET_KEY']
+
+# with open('/etc/secret_key.txt') as f:
+#     SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
