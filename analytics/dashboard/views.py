5bd45d31e79c33cbc01133a83ac7c31fb1f45d8d
Khaled Mebhah <gk_mebhah@esi.dz>
2021-04-28 16:59:45 +0000

    create the pivot_data function

--- analytics/dashboard/views.py
+++ analytics/dashboard/views.py
@@ -3,5 +3,12 @@ from django.shortcuts import render
 from dashboard.models import Order
 from django.core import serializers
 
+# directs a user to the dashboard_with_pivot.html templates
 def dashboard_with_pivot(request):
     return render(request, 'dashboard_with_pivot.html', {})
+
+# sends the response with data to the pivot table on the app's front-end
+def pivot_data(request):
+    dataset = Order.objects.all()
+    data = serializers.serialize('json', dataset)
+    return JsonResponse(data, safe=False)
